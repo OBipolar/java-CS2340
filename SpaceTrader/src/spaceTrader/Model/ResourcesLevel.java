77c986b44da36506cc3abefcd29ef29a91526d1e
neil-lhm <renaissancehang@gmail.com>
2014-09-24 23:55:12 -0400

    universe created Menghang Li

---
+++ SpaceTrader/src/spaceTrader/Model/ResourcesLevel.java
@@ -0,0 +1,16 @@
+package spaceTrader.Model;
+
+public enum ResourcesLevel {
+	NOSPECIAL(0), MINERALPOOR(1), DESERT(2), LOTSOFWAR(3), RICHSOIL(4),
+	POORSOIL(5), RICHFAUNA(6), LIFELESS(7), WEIRDMUSHROOMS(8),
+	LOTSOFHERBS(9), ARTISTIC(10), WARLIKE(11);
+	
+	public static final int NUM_OF_RESOURCES_LEVEL = 12;
+	
+	private int num;
+	
+	ResourcesLevel (int num) {
+		this.num = num;
+	}
+	
+}
