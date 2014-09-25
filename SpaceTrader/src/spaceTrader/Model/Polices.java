93c2e73796b7e4d0f55c09f28d625ceb978b0f17
neil-lhm <renaissancehang@gmail.com>
2014-09-25 00:10:29 -0400

    universe created. police, pirates and government added for every planet. Use toString to print universe info by Menghang Li

---
+++ SpaceTrader/src/spaceTrader/Model/Polices.java
@@ -0,0 +1,13 @@
+package spaceTrader.Model;
+
+public enum Polices {
+	ASKSOMEMONEY(0), ASKALOTMONEY(1), LEAVEYOURSHIP(2), KILLONSIGHT(3);
+	
+	public static final int TYPES_OF_POLICE = 4;
+	
+	private int num;
+	
+	Polices (int num) {
+		this.num = num;
+	}
+}
