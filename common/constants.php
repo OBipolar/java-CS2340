1aecb764c2ded6c193e66d2688cc2e1adcc9c0fd
Haoli Du <henry54809@gmail.com>
2014-09-28 13:40:19 -0400

    STP-8 permission clean up

--- common/constants.php
+++ common/constants.php
@@ -1,8 +1,10 @@
 <?php
-$host = "10.0.1.126";
+//$host = "10.0.1.126";
+$host = "picwo.com";
+$port = "13";
 $dbname = "stp";
 $user = "webuser";
 $password = "8rucShn3t3pew4db";
 
-define("CONNECTION_STRING", "host=$host dbname=$dbname user=$user password=$password");
+define("CONNECTION_STRING", "host=$host dbname=$dbname user=$user password=$password port=$port");
 ?>
