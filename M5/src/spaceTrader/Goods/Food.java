741119895a6c745b351ae961fffc9c523764ad16
neil-lhm <renaissancehang@gmail.com>
2014-10-01 12:14:25 -0400

    javadoc added

---
+++ M5/src/spaceTrader/Goods/Food.java
@@ -0,0 +1,34 @@
+package spaceTrader.Goods;
+
+import spaceTrader.Planets.ResourcesLevel;
+
+/**
+ * Child of Good, Food
+ * 
+ * @author Menghang Li
+ *
+ */
+public class Food extends Good{
+    
+    
+    /**
+     * Constructor thats sets all properties for Food
+     * 
+     */
+    public Food() {
+        setName("Food");
+        setMTLP(1);
+        setMTLU(0);
+        setTTP(1);
+        setBasePrice(100);
+        setIPL(5);
+        setVar(5);
+        setPriceIncrease(PriceIncrease.CROPFAIL.ordinal());
+        setPriceLowCase(ResourcesLevel.RICHSOIL.ordinal());
+        setPriceHighCase(ResourcesLevel.POORSOIL.ordinal());
+        setMinPriceWithTrader(90);
+        setMaxPriceWithTrader(160);
+        
+    }
+    
+}
---
+++ M5/src/spaceTrader/Goods/Machines.java
@@ -0,0 +1,31 @@
+package spaceTrader.Goods;
+
+
+/**
+ * Child of Good, Machines
+ * 
+ * @author Menghang Li
+ *
+ */
+public class Machines extends Good {
+    
+    /**
+     * Constructor thats sets all properties for Machines
+     */
+    public Machines() {
+        setName("Machines");
+        setMTLP(4);
+        setMTLU(3);
+        setTTP(5);
+        setBasePrice(900);
+        setIPL(-30);
+        setVar(5);
+        setPriceIncrease(PriceIncrease.LACKOFWORKERS.ordinal());
+        setPriceLowCase(-1);
+        setPriceHighCase(-1);
+        setMinPriceWithTrader(30);
+        setMaxPriceWithTrader(50);
+        
+    }
+
+}
