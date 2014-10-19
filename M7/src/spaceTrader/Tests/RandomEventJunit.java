daee8957bac8d14a4234c6536414b0f2eec187ce
davidzhu918 <zzhu72@gatech.edu>
2014-10-18 23:59:05 -0400

    M7 added

---
+++ M7/src/spaceTrader/Tests/RandomEventJunit.java
@@ -0,0 +1,22 @@
+package spaceTrader.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import spaceTrader.APIs.RandomEvent;
+
+public class RandomEventJunit {
+
+    private RandomEvent r;
+
+    @Before
+    public void setUp() throws Exception {
+        r = new RandomEvent();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(r.update());
+    }
+
+}
