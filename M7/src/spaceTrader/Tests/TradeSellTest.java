e01193d645f9064d2b125a40418fb1fc25d86a85
davidzhu918 <zzhu72@gatech.edu>
2014-11-12 22:08:44 -0500

    TradeSellTest added

---
+++ M7/src/spaceTrader/Tests/TradeSellTest.java
@@ -0,0 +1,33 @@
+package spaceTrader.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/***
+ * 
+ * @author Zixiang Zhu
+ *
+ */
+
+public class TradeSellTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+
+
